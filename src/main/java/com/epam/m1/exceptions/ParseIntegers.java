e059c4f96eb91f78b481ae978fc6eb310477e50b
Ziyosh <ziyodaakhmedova1998@gmail.com>
2024-03-27 14:10:49 +0500

    try-catch

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -12,17 +12,21 @@ public class ParseIntegers {
 
     private static final List<String> WORDS =
             Arrays.asList(
-                    "JDK 17 has released on 14 September 2021 with 10 new features, 2 feature removals and 2 feature deprecations."
+                    " JDK 17 has released on 14 September 2021 with 10 new features, 2 feature removals and 2 feature deprecations."
                             .split(" "));
 
     public static void main(String[] args) {
         Iterator<String> words = WORDS.iterator();
         int sum = 0;
-        String justWords = "";
+        StringBuilder justWords = new StringBuilder(); //we change String to StringBuilder becuse of append
         while (words.hasNext()) {
             String next = words.next();
-            int number = Integer.parseInt(next);
-            // todo: complete it
+            try{
+                int number = Integer.parseInt(next);
+                sum += number;
+            }catch (NumberFormatException e){
+                justWords.append(next).append(" ");
+            }
         }
         System.out.println("Sum is " + sum);
         System.out.println("Just words:" + justWords);
